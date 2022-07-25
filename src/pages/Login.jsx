366944cad21da8b46f9703a0b3e1f4fbbe8a7110
Avgus <avguswork@gmail.com>
2022-07-25 12:58:53 +0300

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import LoginForm from "../components/LoginForm/LoginForm";
 import Body from "../components/Body/Body";
-import Invoices from "../components/ReactRouter";
-import ReactRouter from "../components/ReactRouter";
 
 const Login = () => {
     return(
         <Body >
             <LoginForm /> 
-            <ReactRouter />
         </Body>
     )
 }
